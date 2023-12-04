00a9a6b4c1c8275a0e216309cad8ead18996c9f9
Jack Preston <jack.preston@smg.team>
2023-12-04 09:30:38 +0000

    day 1 part 2 solution

--- day-1/day-1.js
+++ day-1/day-1.js
@@ -3,15 +3,37 @@ const fs = require('fs');
 // read lines of input text in to array
 const input = fs.readFileSync('day-1/input.txt', 'utf8').split('\n');
 
-// filter out non-numeric characters
+const numberWordMap = new Map([
+    ['one', '1'],
+    ['two', '2'],
+    ['three', '3'],
+    ['four', '4'],
+    ['five', '5'],
+    ['six', '6'],
+    ['seven', '7'],
+    ['eight', '8'],
+    ['nine', '9'],
+])
+
 const numbers = input.map((line) => {
-    const numbersOnly = line.replace(/\D/g, '')
 
-    // If only 1 number, concatenate string with itself. Otherwise take first and last number.
-    const firstAndLastNumber = numbersOnly.length === 1
-        ? numbersOnly + numbersOnly
-        : numbersOnly[0] + numbersOnly[numbersOnly.length - 1];
+    // Find all matches using lookahead
+    const matches = Array.from(
+        line.matchAll(/(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))/g),
+        // First match with a lookahead is the void before the target string, so take the second match
+        match => match[1],
+    );
 
+    const firstMatch = isNaN(Number(matches[0]))
+        ? numberWordMap.get(matches[0])
+        : matches[0];
+
+    const lastMatch = isNaN(Number(matches[matches.length - 1]))
+        ? numberWordMap.get(matches[matches.length - 1])
+        : matches[matches.length - 1];
+
+    const firstAndLastNumber = firstMatch + lastMatch;
+    
     if (firstAndLastNumber.length !== 2) {
         throw new Error('First and last numbers should be length 2');
     }
@@ -19,6 +41,9 @@ const numbers = input.map((line) => {
     return parseInt(firstAndLastNumber);
 });
 
-const total = numbers.reduce((sum, number) => sum + number);
+const total = numbers.reduce((sum, number, index) => {
+    console.log(index);
+    return sum + number;
+});
 
 console.log(total);
